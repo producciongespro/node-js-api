ca8f6996f8b3b19d1c4f496ff2c5be282a14a6bc
lchaconc <luis.chacon.campos@gmail.com>
2021-03-12 13:33:27 -0600

    Imp varibla de entrono fase 1
    -instalación dedotenv
    -creación de .env
    -creaci´pon de varibal URI de mongodb

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 
 const start = async () => {
-    const db = await mongoose.connect('mongodb://localhost/valle-fresco', {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
